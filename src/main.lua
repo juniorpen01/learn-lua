8db2b6c2049b3c7be13851f8bf947e2b2a8bea53
juniorpen01 <01juniorpen@gmail.com>
2025-12-06 17:35:30 +0800

    Implement guessing game (with actually barely half-decent code)

--- src/main.lua
+++ src/main.lua
@@ -1,80 +1,87 @@
 -- this uses vim/neovim typa shi for some reason
 
-local MIN_RANDOM_NUMBER = 1
-local MAX_RANDOM_NUMBER = 100
+local MIN = 1
+local MAX = 100
+
+local INTRO = string.format(
+	[[Number Guessing Game
+
+Guess a number from %d-%d! Let's see how efficient you are!
+
+]],
+	MIN,
+	MAX
+)
+local PLAY_PROMPT = "Do you want to play?"
+local GUESS_PROMPT = string.format("Pick an integer from (%d to %d): ", MIN, MAX)
+
+---@param prompt string
+local function getInput(prompt)
+	local input = vim.fn.input(prompt)
+	vim.print("\n")
+	return input
+end
+
+---@param prompt string
+local function askYesOrNo(prompt)
+	while true do
+		local input = string.upper(getInput(prompt .. " (Y/n): "))
+		if input == "" or input == "Y" then
+			return true
+		elseif input == "N" then
+			return false
+		end
+	end
+end
+
+---@param prompt string
+---@return integer
+local function getInt(prompt)
+	while true do
+		local input = getInput(prompt)
+
+		local foo = tonumber(input)
+		if foo then
+			local bar = math.floor(foo)
+			if foo == bar then
+				return bar
+			end
+		end
+	end
+end
+
+---@param prompt string
+local function getGuess(prompt)
+	while true do
+		local guess = getInt(prompt)
+		if guess < MIN or guess > MAX then
+			vim.print("Really? You were already told the range. Do better!")
+		else
+			return guess
+		end
+	end
+end
+
+vim.print(INTRO)
+
+if not askYesOrNo(PLAY_PROMPT) then
+	return
+end
 
 math.randomseed(os.time())
 
-print(string.format(
-	[[Number Guessing Game
-
-Guess a number from %d-%d! Let's see how efficient you are.
-
-]],
-	MIN_RANDOM_NUMBER,
-	MAX_RANDOM_NUMBER
-))
+local random = math.random(MIN, MAX)
 
 while true do
-	local isPlaying
-	while true do
-		local input = string.upper(vim.fn.input("Do you want to play? (Y/n): "))
-		if input == "" or input == "Y" then
-			isPlaying = true
-			break
-		elseif input == "N" then
-			isPlaying = false
-			break
-		end
-	end
-
-	if isPlaying then
-		local randomNumber = math.random(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
-
-		while true do
-			local guessNumber
-			while true do
-				local input =
-					vim.fn.input(string.format("Guess a number from %d-%d!: ", MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER))
-				print("\n")
-
-				local value = tonumber(input)
-				if value < MIN_RANDOM_NUMBER or value > MAX_RANDOM_NUMBER then
-					print(
-						string.format(
-							"We already told you to guess from %d-%d! Do better!",
-							MIN_RANDOM_NUMBER,
-							MAX_RANDOM_NUMBER
-						)
-					)
-				else
-					guessNumber = value
-					break
-				end
-			end
-
-			if guessNumber < randomNumber then
-				print("Too low!")
-			elseif guessNumber > randomNumber then
-				print("Too high!")
-			else
-				print("Equal!")
-				break
-			end
-		end
-		while true do
-			local input = string.upper(vim.fn.input("Do you want to play again? (Y/n): "))
-			if input == "" or input == "Y" then
-				isPlaying = true
-				break
-			elseif input == "N" then
-				isPlaying = false
-				break
-			end
-		end
+	local guess = getGuess(GUESS_PROMPT)
+	if guess < random then
+		vim.print("higher!")
+	elseif guess > random then
+		vim.print("lower!")
 	else
+		vim.print("nice!")
 		break
 	end
 end
 
-print("\nThanks for playing!")
+-- i am not doing "replay" just to express my dissent with this code bruh
